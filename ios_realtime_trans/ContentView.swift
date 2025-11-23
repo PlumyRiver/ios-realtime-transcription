36917c24cd807867e9b510efaf31066b81767267
plumyriver <east6413@gmail.com>
2025-11-23 13:49:51 +0800

    Initial Commit

---
+++ ios_realtime_trans/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios_realtime_trans
+//
+//  Created by 吳東隆 on 2025/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios_realtime_trans/ios_realtime_transApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_realtime_transApp.swift
+//  ios_realtime_trans
+//
+//  Created by 吳東隆 on 2025/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_realtime_transApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
